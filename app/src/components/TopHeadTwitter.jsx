6a066172b25f5cadbc7135efee0b74278d894a83
Karim <karim.zouli.edu@gmail.com>
2023-05-10 16:39:53 +0200

    Amélioration du visuel et ajout de l'entete

--- app/src/components/TopHeadTwitter.jsx
+++ app/src/components/TopHeadTwitter.jsx
@@ -4,10 +4,14 @@ const TopHeadTwitter = () => {
   return (
     <div className="container-tophead">
         <p>Home</p>
-        <div className="switch-subject">
-            <button>For you</button>
-            <button>Following</button>
+      <div className="switch-subject">
+        <div className="switch-foryou">
+            <a href="#">For you</a>
         </div>
+        <div className="switch-following">
+            <a href="#">Following</a>
+        </div>
+      </div>
     </div>
   )
 }
